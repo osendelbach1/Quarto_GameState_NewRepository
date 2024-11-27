91c4f4947d2cfd78d2b81d7aa1d4f68b90505983
osendelbach1 <144176726+osendelbach1@users.noreply.github.com>
2024-11-27 13:09:43 -0800

    Update QuitGameAction.java
    Added quitGameAction class

--- app/src/main/java/edu/up/cs301/quarto/QuitGameAction.java
+++ app/src/main/java/edu/up/cs301/quarto/QuitGameAction.java
@@ -1,5 +1,36 @@
 package edu.up.cs301.quarto;
 
-public class QuitGameAction {
-    //fill in class here
+import edu.up.cs301.GameFramework.actionMessage.GameAction;
+import edu.up.cs301.GameFramework.players.GamePlayer;
+
+import java.io.Serializable;
+
+
+/**
+ * This contains class declaration for the QuitGameAction. This class consist of a GamePlayer,
+ * a canQuitAction that will always return true so that you can always quit the game, and the quitAction.
+ * @author Olivia Sendelbach, Magnus Graham, Becca Biukoto
+ * Code Reference: Qwirkle QuitGameAction Class
+ * @Date 11/27/24
+ */
+public class quitGameAction extends GameAction implements Serializable
+{
+    private static final long serialVersionUID = 1126202472;
+
+    public quitGameAction(GamePlayer player)
+    {
+        super(player);
+    }
+
+    //Makes it so you can always quit
+    public boolean canQuitAction()
+    {
+        return true;
+    }
+
+    public void quitAction()
+    {
+        GamePlayer GamePlayer = this.getPlayer();
+        System.exit(0);
+    }
 }
